f6437bd1ffebb0dd39a49875bcc9960a6951a32c
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-02-20 14:38:58 +0200

    Adding code examples/build_curl.sh

---
+++ code examples/build_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal_ecommerce", "url": "https://www.build.com/bathroom/c108412"}'
